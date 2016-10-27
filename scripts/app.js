e3503771eaaac3dd7820a157383eabcf4d88e773
Michael <mpmolinari14@gmail.com>
2016-10-27 10:32:08 -0700

    responsive updates

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(document).ready(function() {
+
+  $('#header-menu').on('click', function() {
+    $('#main-nav').toggleClass('open-nav');
+  });
+
+});
--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,58 +1,41 @@
 'use strict';
 
+var projects = [];
 
-$(document).ready(function() {
-  var projects = [];
+function Project (opts) {
+  this.projName = opts.projName;
+  this.developers = opts.developers;
+  this.deployedOn = opts.deployedOn;
+  this.projImage = opts.projImage;
+  this.projRepo = opts.projRepo;
+  this.projURL = opts.projURL;
+  this.description = opts.description;
+};
 
-  function Project (opts) {
-    this.projName = opts.projName;
-    this.developers = opts.developers;
-    this.deployedOn = opts.deployedOn;
-    this.projImage = opts.projImage;
-    this.projRepo = opts.projRepo;
-    this.projURL = opts.projURL;
-    this.description = opts.description;
-  };
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone().removeClass();
+  $newProject.find('h1').html(this.projName);
+  $newProject.find('address a').html(this.developers);
+  $newProject.find('time').html(this.deployedOn);
+  $newProject.find('img').attr('src', this.projImage);
+  $newProject.find('#repo-button').attr('href', this.projRepo);
+  $newProject.find('#url-button').attr('href', this.projURL);
+  $newProject.find('.description').html(this.description);
 
-  Project.prototype.toHtml = function() {
-    var $newProject = $('article.template').clone().removeClass();
-    $newProject.find('h1').html(this.projName);
-    $newProject.find('address a').html(this.developers);
-    $newProject.find('time').html(this.deployedOn);
-    $newProject.find('img').attr('src', this.projImage);
-    $newProject.find('#repo-button').attr('href', this.projRepo);
-    $newProject.find('#url-button').attr('href', this.projURL);
-    $newProject.find('.description').html(this.description);
+  $newProject.find('time[pubdate]').attr('title', this.deployedOn);
+  $newProject.find('time').text('about ' + parseInt((new Date() - new Date(this.deployedOn))/60/60/24/1000) + ' days ago');
 
-    $newProject.find('time[pubdate]').attr('title', this.deployedOn);
-    $newProject.find('time').text('about ' + parseInt((new Date() - new Date(this.deployedOn))/60/60/24/1000) + ' days ago');
+  return $newProject;
+};
 
-    return $newProject;
-  };
+projectData.sort(function(currentObject, nextObject) {
+  return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
+});
 
-  projectData.sort(function(currentObject, nextObject) {
-    return (new Date(nextObject.publishedOn)) - (new Date(currentObject.publishedOn));
-  });
+projectData.forEach(function(ele) {
+  projects.push(new Project(ele));
+});
 
-  projectData.forEach(function(ele) {
-    projects.push(new Project(ele));
-  });
-
-  projects.forEach(function(project) {
-    $('#projects').append(project.toHtml());
-  });
-
-  // HIDE OTHER PAGES ON LOAD //
-  $('#projects').hide();
-
-  // SHOW PORTFOLIO PAGE //
-  $('#portfolio-button').on('click', function() {
-    $('#resume-button, #portfolio-button, .name-cont').hide();
-    $('body').css('background', 'none');
-    $('.main-header, footer').css('background-color', 'rgba(0,0,0,0.8)');
-    $('#projects').show();
-  });
-
-
-
+projects.forEach(function(project) {
+  $('#portfolio').append(project.toHtml());
 });
